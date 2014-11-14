b4c905bb605a5de70da35f690bdb46950da42f4d
robertandrew <andrewvandam@gmail.com>
2014-11-13 22:55:41 -0500

    Create script2.js

---
+++ gross/script2.js
@@ -0,0 +1,49 @@
+(function() {
+
+	$(function() {
+
+		//////////////////////////////
+		/////OPEN/////////////////////
+
+	
+		//////////////////////////////
+		/////GLOBAL VARIABLES/////////
+		//////////////////////////////
+		
+		var	
+				//hard-code the heights
+				qHeight = 120, //constant
+				wfBarHeight = 15, //constant
+				Duration = 500, //constant
+				neg = 
+				
+		
+		
+		//Quarterly chart -- known as "q"
+		function drawQuarters(){
+		
+		}//close drawQuarters
+		
+		//waterfall chart -- known as "wf"
+		function drawWaterfall(){
+		
+		}		
+		
+		//just pulling in data		
+		function getData(){
+		
+		}//close GetData
+		
+		
+		
+		
+		
+		//////CLOSE MY CALLS/////////
+		//////////////////////////////	
+	
+		var fm = Iframe.init(); // must be at the end of your code
+	});
+
+	socialRiser.create();
+
+})();
